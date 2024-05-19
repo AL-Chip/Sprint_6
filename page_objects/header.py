6bcc8c98f735ff2ab11b98da5be3b9f19e31f8e1
Alecsandr Lezin <alecsandr.lezin@mail.ru>
2024-05-19 17:48:17 +0300

    add header_locators

--- page_objects/header.py
+++ page_objects/header.py
@@ -1,18 +1,14 @@
 from page_objects.base_page import BasePage
-from selenium.webdriver.common.by import By
+from locators.header_locators import HeaderLocators
 
 
 class Header(BasePage):
 
-    _BUTTON_ORDER_HEADER = By.XPATH, "//*[contains(@class, 'Header_Nav')]/button[text() = 'Заказать']" # Кнопка заказа\
-    _LOGO_SCOOTER = By.XPATH, "//*[contains(@class, 'Header_LogoScooter')]"
-    _LINK_YANDEX = By.XPATH, "//*[contains(@class, 'Header_LogoYandex')]"
-
     def click_button_order(self):
-        self.driver.find_element(*self._BUTTON_ORDER_HEADER).click()
+        self.driver.find_element(*HeaderLocators.BUTTON_ORDER_HEADER).click()
 
     def click_link_logo(self):
-        self.driver.find_element(*self._LOGO_SCOOTER).click()
+        self.driver.find_element(*HeaderLocators.LOGO_SCOOTER).click()
 
     def click_link_yandex(self):
-        self.driver.find_element(*self._LINK_YANDEX).click()
+        self.driver.find_element(*HeaderLocators.LINK_YANDEX).click()
